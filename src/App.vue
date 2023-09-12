4925d1da9e45636df376202f2167bde073dea1e5
umbertofaenza <umberto.faenza@gmail.com>
2023-09-12 15:16:36 +0200

    edit: remove wrong code

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 
-import Bootstrap from "";
-
 export default {
   components: { AppHeader, AppMain },
 };
