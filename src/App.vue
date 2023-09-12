25bee14f2bb1492c7470b591a01d05815707fe02
umbertofaenza <umberto.faenza@gmail.com>
2023-09-12 17:23:54 +0200

    feat: implement store to fetch, save, access data

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,30 @@
 <script>
+import axios from "axios";
+import { store } from "./data/store";
+
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 
 export default {
+  data() {
+    return {
+      store,
+    };
+  },
+
+  methods: {
+    fetchCards(endpoint) {
+      axios.get(endpoint).then((response) => {
+        store.cards = response.data.data;
+      });
+    },
+  },
+
+  created() {
+    this.fetchCards(
+      "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0"
+    );
+  },
   components: { AppHeader, AppMain },
 };
 </script>
--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -1,37 +1,22 @@
 <script>
-import axios from "axios";
-
+import { store } from "../data/store";
 import Card from "./Card.vue";
 
 export default {
   data() {
     return {
-      cards: [],
+      store,
     };
   },
 
   components: { Card },
-
-  methods: {
-    fetchCards(endpoint) {
-      axios.get(endpoint).then((response) => {
-        this.cards = response.data.data;
-      });
-    },
-  },
-
-  created() {
-    this.fetchCards(
-      "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0"
-    );
-  },
 };
 </script>
 
 <template>
   <div class="container py-5">
     <div class="row p-4 g-3 row-cols-2 row-cols-md-3 row-cols-lg-5">
-      <Card v-for="card in cards" :key="card.id" :card="card" />
+      <Card v-for="card in store.cards" :key="card.id" :card="card" />
     </div>
   </div>
 </template>
