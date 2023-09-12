7329272c16e6c367ae4ce6b787a8d8c3b77663b5
umbertofaenza <umberto.faenza@gmail.com>
2023-09-12 15:21:07 +0200

    feat: add title to header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,17 @@
 <script></script>
 
 <template>
-  <h1>App Header</h1>
+  <header>
+    <div class="container">
+      <h1>Yu-Gi-Oh API</h1>
+    </div>
+  </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  h1 {
+    padding: 10px 0;
+  }
+}
+</style>
